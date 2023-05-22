914e68ec7876347abda43601b8ed56da1f1784a0
Hassan Qadir <77539962+hassan-qadir@users.noreply.github.com>
2023-05-22 13:08:00 +0500

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -12,13 +12,13 @@ const Page = () => {
     <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
       <div>
         <label for="first-name" className="block text-sm font-semibold leading-6 text-gray-900">First name</label>
-        <div className="mt-2.5">
+        <div className="mt-2.6">
           <input type="text" name="first-name" id="first-name" autocomplete="given-name" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
       </div>
       <div>
         <label for="last-name" className="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
-        <div className="mt-2.5">
+        <div className="mt-2.6">
           <input type="text" name="last-name" id="last-name" autocomplete="family-name" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
       </div>
